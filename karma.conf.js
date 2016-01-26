a561bd5c1a2b774c480ae64cd965c387a216db47
Gabin <hello@gabinaureche.com>
2016-01-26 22:01:02 +0100

    convert tests to karma+jasmine+webpack

--- karma.conf.js
+++ karma.conf.js
@@ -1,42 +1,47 @@
-'use strict';
-// Karma configuration
-// Generated on Mon Jun 16 2014 13:09:52 GMT+0100 (BST)
-
 module.exports = function (config) {
   config.set({
-
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'browserify'],
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      'index.js': 'browserify',
-      'test/index.test.js': 'browserify'
-    },
+    frameworks: [
+      'jasmine'
+    ],
 
     // list of files / patterns to load in the browser
     files: [
-      'node_modules/jquery/dist/jquery.min.js',
-      'node_modules/chai/chai.js',
-      'node_modules/sinon/pkg/sinon-1.10.1.js',
-      'node_modules/sinon-chai/lib/sinon-chai.js',
-
-      'test/index.test.js',
-      'index.js'
+      'tests/*.spec.js'
     ],
+
+    webpack: {
+      module: {
+        loaders: [
+          {
+            test: /\.js$/,
+            loaders: ['babel-loader']
+          }
+        ]
+      }
+    },
+
+    webpackServer: {
+      noInfo: true
+    },
 
     // list of files to exclude
     exclude: [],
 
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      'tests/*.spec.js': ['webpack']
+    },
+
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['dots'],
+    reporters: ['progress'],
 
     // web server port
     port: 9876,
@@ -49,14 +54,23 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
 
     // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
+    autoWatch: true,
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true
-  });
-};
+    singleRun: false,
+
+    plugins: [
+      require('karma-webpack'),
+      require('karma-jasmine'),
+      require('karma-phantomjs-launcher')
+    ]
+  })
+}
---
+++ tests/index.spec.js
@@ -0,0 +1,459 @@
+/* global describe, beforeEach, it, expect */
+
+require('../src/index')
+
+function isValid (validity) {
+  for (let key in validity) {
+    const value = validity[key]
+
+    if (key === 'valid') {
+      if (value === false) return false
+    } else if (value === true) {
+      return false
+    }
+  }
+
+  return true
+}
+
+describe('HTML5 validation', function () {
+  describe('input[type=text]', function () {
+    let input
+
+    beforeEach(function () {
+      input = document.createElement('input')
+      input.setAttribute('type', 'text')
+    })
+
+    it('should be valid if empty and not required', function () {
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+
+    it('should be valid if not empty and not required', function () {
+      input.value = 'foo'
+
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+
+    it('should be valid if not empty and required', function () {
+      input.setAttribute('required', '')
+      input.value = 'foo'
+
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+
+    it('should be invalid if empty and required', function () {
+      input.setAttribute('required', '')
+
+      expect(input.validity.valueMissing).toBe(true)
+      expect(input.validity.valid).toBe(false)
+      expect(input.checkValidity()).toBe(false)
+    })
+
+    it('should be valid if disabled', function () {
+      input.setAttribute('required', '')
+      input.setAttribute('disabled', '')
+
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+
+    it('should be valid if value matches [pattern]', function () {
+      input.setAttribute('pattern', '[a-z]{1,3}%')
+      input.value = 'abc%'
+
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+
+    it('should be invalid if value doesn\'t match [pattern]', function () {
+      input.setAttribute('pattern', '[a-z]{1,3}%')
+      input.value = '123456'
+
+      expect(input.validity.patternMismatch).toBe(true)
+      expect(input.validity.valid).toBe(false)
+      expect(input.checkValidity()).toBe(false)
+    })
+  })
+
+  describe('input[type=checkbox]', function () {
+    let input
+
+    beforeEach(function () {
+      input = document.createElement('input')
+      input.setAttribute('type', 'checkbox')
+    })
+
+    it('should be valid if not checked and not required', function () {
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+
+    it('should be valid if checked and not required', function () {
+      input.setAttribute('checked', '')
+
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+
+    it('should be valid if checked and required', function () {
+      input.setAttribute('required', '')
+      input.setAttribute('checked', '')
+
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+
+    it('should be invalid if not checked and required', function () {
+      input.setAttribute('required', '')
+
+      expect(input.validity.valueMissing).toBe(true)
+      expect(input.validity.valid).toBe(false)
+      expect(input.checkValidity()).toBe(false)
+    })
+  })
+
+  describe('input[type=radio]', function () {
+    let input
+
+    beforeEach(function () {
+      input = document.createElement('input')
+      input.setAttribute('type', 'radio')
+    })
+
+    it('should be valid if not checked and not required', function () {
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+
+    it('should be valid if checked and not required', function () {
+      input.setAttribute('checked', '')
+
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+
+    it('should be valid if checked and required', function () {
+      input.setAttribute('required', '')
+      input.setAttribute('checked', '')
+
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+
+    it('should be valid if not checked and required', function () {
+      input.setAttribute('required', '')
+
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+  })
+
+  describe('input[type=tel]', function () {
+    let input
+
+    beforeEach(function () {
+      input = document.createElement('input')
+      input.setAttribute('type', 'tel')
+    })
+
+    it('should be valid if filled with a number', function () {
+      input.value = '123'
+
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+
+    it('should be valid if filled with a string', function () {
+      input.value = 'abc'
+
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+
+    it('should be valid if empty and not required', function () {
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+
+    it('should be valid if not empty and not required', function () {
+      input.value = 'foo'
+
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+
+    it('should be valid if not empty and required', function () {
+      input.setAttribute('required', '')
+      input.value = 'foo'
+
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+
+    it('should be invalid if empty and required', function () {
+      input.setAttribute('required', '')
+
+      expect(input.validity.valueMissing).toBe(true)
+      expect(input.validity.valid).toBe(false)
+      expect(input.checkValidity()).toBe(false)
+    })
+
+    it('should be valid if disabled', function () {
+      input.setAttribute('required', '')
+      input.setAttribute('disabled', '')
+
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+
+    it('should be valid if value matches [pattern]', function () {
+      input.setAttribute('pattern', '[a-z]{1,3}%')
+      input.value = 'abc%'
+
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+
+    it('should be invalid if value doesn\'t match [pattern]', function () {
+      input.setAttribute('pattern', '[a-z]{1,3}%')
+      input.value = '123456'
+
+      expect(input.validity.patternMismatch).toBe(true)
+      expect(input.validity.valid).toBe(false)
+      expect(input.checkValidity()).toBe(false)
+    })
+  })
+
+  describe('input[type=number]', function () {
+    let input
+
+    beforeEach(function () {
+      input = document.createElement('input')
+      input.setAttribute('type', 'number')
+    })
+
+    it('should be valid if filled with a positive number', function () {
+      input.value = '123'
+
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+
+    it('should be valid if filled with a negative number', function () {
+      input.value = '-123'
+
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+
+    it('should be invalid if filled with a decimal number', function () {
+      input.value = '1.23'
+
+      expect(input.validity.typeMismatch).toBe(true)
+      expect(input.validity.valid).toBe(false)
+      expect(input.checkValidity()).toBe(false)
+    })
+
+    it('should be valid if empty and not required', function () {
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+
+    it('should be valid if not empty and not required', function () {
+      input.value = 'foo'
+
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+
+    it('should be valid if not empty and required', function () {
+      input.setAttribute('required', '')
+      input.value = 'foo'
+
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+
+    it('should be invalid if empty and required', function () {
+      input.setAttribute('required', '')
+
+      expect(input.validity.valueMissing).toBe(true)
+      expect(input.validity.valid).toBe(false)
+      expect(input.checkValidity()).toBe(false)
+    })
+
+    it('should be valid if value is greater than min', function () {
+      input.setAttribute('min', '10')
+      input.value = '15'
+
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+
+    it('should be invalid if value is lower than min', function () {
+      input.setAttribute('min', '10')
+      input.value = '5'
+
+      expect(input.validity.rangeUnderflow).toBe(true)
+      expect(input.validity.valid).toBe(false)
+      expect(input.checkValidity()).toBe(false)
+    })
+
+    it('should be valid if value is lower than max', function () {
+      input.setAttribute('max', '10')
+      input.value = '5'
+
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+
+    it('should be invalid if value is greater than max', function () {
+      input.setAttribute('max', '10')
+      input.value = '15'
+
+      expect(input.validity.rangeOverflow).toBe(true)
+      expect(input.validity.valid).toBe(false)
+      expect(input.checkValidity()).toBe(false)
+    })
+  })
+
+  describe('input[type=range]', function () {
+    let input
+
+    beforeEach(function () {
+      input = document.createElement('input')
+      input.setAttribute('type', 'number')
+    })
+
+    it('should be valid if filled with a number', function () {
+      input.value = '123'
+
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+
+    it('should be valid if filled with a string', function () {
+      input.value = 'abc'
+
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+
+    it('should be invalid if empty', function () {
+      expect(input.validity.valid).toBe(false)
+      expect(input.checkValidity()).toBe(false)
+    })
+  })
+
+  describe('input[type=email]', function () {
+    let input
+
+    beforeEach(function () {
+      input = document.createElement('input')
+      input.setAttribute('type', 'number')
+    })
+
+    it('should be valid if filled with a correct email address', function () {
+      input.value = 'email@domain.com'
+
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+
+    it('should be invalid if filled with an incorrect email address', function () {
+      input.value = 'foo'
+
+      expect(input.validity.typeMismatch).toBe(true)
+      expect(input.validity.valid).toBe(false)
+      expect(input.checkValidity()).toBe(false)
+    })
+  })
+
+  describe('input[type=url]', function () {
+    let input
+
+    beforeEach(function () {
+      input = document.createElement('input')
+      input.setAttribute('type', 'url')
+    })
+
+    it('should be valid if filled with a correct url', function () {
+      input.value = 'https://site.com'
+
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+
+    it('should be invalid if filled with an incorrect url', function () {
+      input.value = 'foo'
+
+      expect(input.validity.typeMismatch).toBe(true)
+      expect(input.validity.valid).toBe(false)
+      expect(input.checkValidity()).toBe(false)
+    })
+  })
+
+  describe('select', function () {
+    it('should be valid if not empty')
+  })
+
+  describe('textarea', function () {
+    let input
+
+    beforeEach(function () {
+      input = document.createElement('textarea')
+    })
+
+    it('should be valid if empty and not required', function () {
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+
+    it('should be valid if not empty and not required', function () {
+      input.value = 'foo'
+
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+
+    it('should be valid if not empty and required', function () {
+      input.setAttribute('required', '')
+      input.value = 'foo'
+
+      expect(isValid(input.validity)).toBe(true)
+      expect(input.checkValidity()).toBe(true)
+    })
+
+    it('should be invalid if empty and required', function () {
+      input.setAttribute('required', '')
+
+      expect(input.validity.valueMissing).toBe(true)
+      expect(input.validity.valid).toBe(false)
+      expect(input.checkValidity()).toBe(false)
+    })
+  })
+
+  describe('checkValidity', function () {
+    it('should emit an event when called')
+  })
+
+  describe('setCustomValidity', function () {
+    let input
+
+    beforeEach(function () {
+      input = document.createElement('input')
+    })
+
+    it('should set input.validationMessage property', function () {
+      const customMessage = 'This is a custom validation message'
+
+      expect(input.validationMessage).not.toBe(customMessage)
+      input.setCustomValidity(customMessage)
+      expect(input.validationMessage).toBe(customMessage)
+    })
+  })
+})
